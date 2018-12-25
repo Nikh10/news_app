cf925641b4a4fe0c607f83d9b893b40602a9777f
nikhilkumar <nikhilkumarhiragond@gmail.com>
2018-12-25 18:21:48 +0530

    modification to wsgi file

--- newsapp/wsgi.py
+++ newsapp/wsgi.py
@@ -10,10 +10,12 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsapp.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
-# 
+# application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
